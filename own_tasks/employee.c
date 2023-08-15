28a93d622b7fd85ab2e7e677fa4bc774d8aa6315
Katherine <katherinevse@mail.ru>
2023-08-15 11:24:25 +0300

    -

--- own_tasks/employee.c
+++ own_tasks/employee.c
@@ -1,6 +1,4 @@
 /*
-
-Конечно, вот ваша задача для создания структуры на языке Си:
 Задача: Учет сотрудников и анализ стажа работы
 Требуется разработать программу на языке Си для учета данных о сотрудниках компании и анализа их стажа работы.
 Каждый сотрудник описывается следующими данными:
@@ -10,7 +8,9 @@
 Отчество
 Должность
 Пол
-Дата приема на работу (в формате ДД.ММ.ГГГГ)
+Год приема на работу
+
+
 Необходимо выполнить следующие шаги:
 
 Создать структуру Employee, включающую все указанные данные.
@@ -19,7 +19,7 @@
 Реализовать функцию printExperiencedEmployees, которая принимает массив структур Employee, 
 их количество и средний стаж работы, и выводит на экран информацию о сотрудниках, стаж которых выше среднего.
 
-Программа должна запрашивать у пользователя данные о сотрудниках (не более 100) и выводить результат анализа на экран.
+Программа должна запрашивать у пользователя данные о сотрудниках и выводить результат анализа на экран.
 
 */ 
 
@@ -29,12 +29,56 @@
 #define MAX_EMPLOYEES 100
 
 struct Employee {
-    char lastName[50];
-    char firstName[50];
-    char middleName[50];
+    char name[50];
+    char surname[50];
     char position[50];
     char gender;
     int yearsOfService;
+};
+
+float calculateAverageExperience(struct Employee employers[], int numEmployers)
+{
+    int totalExperience = 0;
+    for (int i = 0; i < numEmployers; i++)
+    {
+        totalExperience += employers[i].yearsOfService;
+    }
+    return (float) totalExperience / numEmployers;
     
 };
+
+// печатать только тех, у кого больше опыта, чем average;
+void printExperiencedEmployees(struct Employee employers[],int numEmpoyers, float totalExperience)
+{
+    for (int i = 0; i < numEmpoyers; i++)
+    {
+        if (employers[i].yearsOfService > averageExperience)
+        {
+            
+        }
+        
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+int main (void)
+{
+    struct Employee employers[MAX_EMPLOYEES];
+    int numEmployers = 2; 
+    
+
+
+    float averageExperience = calculateAverageExperience(employers, numEmployers);
+}
 
