ec2d1dd6f58bb85084225eb8360240ad2ad244c8
Katherine <katherinevse@mail.ru>
2023-10-16 19:56:53 +0300

    -

---
+++ leet_code/even_numbers.c
@@ -0,0 +1,27 @@
+/*
+Ввод: числа = [0,1,0,3,12]
+Вывод: [1,3,12,0,0]
+*/
+
+#include <stdio.h>
+
+void evenNum(int* nums, int numsSize){
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (nums[i] % 2 == 0 ){
+            printf( "%d", nums[i]);
+        }
+    }
+}
+
+int main(){
+    int arr [] = {1, 2, 4, 5, 8};
+    int size = sizeof(arr)/ sizeof(arr[0]);
+
+    evenNum(arr,size);
+
+
+
+    return 0;
+}
---
+++ leet_code/sum_numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void sumNum(int *numbers, int sizeOf){
+
+    int sum = 0;
+
+    for (int i = 0; i < sizeOf; i++)
+    {
+        sum+= numbers[i];
+        
+    }
+    printf("%d", sum);
+}
+
+int main(){
+
+    int array[] = {3, 3, 3};
+    int size = sizeof(array)/ sizeof(array[0]);
+
+
+    sumNum(array, size);
+
+    return 0;
+}
