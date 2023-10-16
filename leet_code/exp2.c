1c5784f26fe1519ec96cf53f9c0ac6955ed8ee1c
Katherine <katherinevse@mail.ru>
2023-10-16 18:50:24 +0300

    -

---
+++ leet_code/exp2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Функция для нахождения суммы элементов массива
+int findSum(int *arr, int size) {
+    int sum = 0;
+
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    // Передаем массив и его размер в функцию
+    int result = findSum(numbers, size);
+
+    printf("Сумма элементов массива: %d\n", result);
+
+    return 0;
+}
+
+//arr на что указывает? 
+// что принимает?
---
+++ leet_code/move_zeros.c
@@ -0,0 +1,23 @@
+/*
+Ввод: числа = [0,1,0,3,12]
+Вывод: [1,3,12,0,0]
+*/
+
+#include <stdio.h>
+
+void moveZeroes(int* nums, int numsSize){
+
+}
+
+int main(){
+    int nums;
+    scanf("%d", &nums);
+
+    for (int i = 0; i < size; i++)
+    {
+        
+    }
+    
+
+
+}
