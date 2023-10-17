bdc02558632f241465097fe9cbc8f334d7589b4e
Katherine <katherinevse@mail.ru>
2023-10-17 11:20:04 +0300

    -

--- leet_code/max_numbers.c
+++ leet_code/max_numbers.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+void maxNum(int* numbers, int sizeOf) {
+    int state = 0;
+
+    for (int i = 0; i < sizeOf; i++) {
+        if (state < numbers[i]) {
+            state = numbers[i];
+        }
+    }
+    printf("%d", state);
+}
+
+int main() {
+    int arr[] = {3, 6, 2, 100, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    maxNum(arr, size);
+
+    return 0;
+}
