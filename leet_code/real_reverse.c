948488291fbd9907a4211b510ddf23d9feee7bf3
Katherine <katherinevse@mail.ru>
2023-10-17 15:08:12 +0300

    -

--- leet_code/real_reverse.c
+++ leet_code/real_reverse.c
@@ -2,29 +2,25 @@
 
 void reverseArray(int *num, int size) {
     int temp = 0;
-
-    for (int i = 0; i < size; i++) {
-        for (int j = size - 1; j >= 0; j--) {
-            temp = num[i];    // 1
-            num[i] = num[j];  // i = 9
-            num[j] = temp;
-        }
-    }
-
-    for (int t = 0; t < size; t++) {
-        printf("%d", num[t]);
+    int i = 0;
+    int j = size - 1;
+    while (i < j) {
+        temp = num[i];    // 1
+        num[i] = num[j];  // i = 9
+        num[j] = temp;
+        i++;
+        j--;
     }
 }
-//
-/*
-1 = index size
-2 = index size -1
-3 = index size -2 ( j ) - index
-*/
+
 int main() {
     int arr[] = {1, 5, 6, 7, 8, 9};
 
     int size = sizeof(arr) / sizeof(arr[0]);
 
     reverseArray(arr, size);
+    for (int t = 0; t < size; t++) {
+        printf("%d", arr[t]);
+    }
+    return 0;
 }
