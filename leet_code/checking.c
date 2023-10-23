5ce6ed711620c1f6589bb33d5aaae8e214dacafb
Katherine <katherinevse@mail.ru>
2023-10-23 10:46:07 +0300

    -

--- leet_code/checking.c
+++ leet_code/checking.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+void printCharacters(char str[]) {
+    printf("%c", str[0]);
+    
+}
+
+int main() {
+    char myString[] = "Hello";
+    printCharacters(myString);
+
+    return 0;
+}
+
+// вот это равно этому
+
+
 void printCharacters(char *str) {
-        printf("%c", *str);
+    printf("%c", *str);
     
 }
 
